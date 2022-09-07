75e10fe6be35d018349fb9422bc3dab0ebbd041a
quint-fain <quintdf@me.com>
2022-09-07 09:33:16 -0400

    Initial Commit

---
+++ Art App/Art_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_AppApp.swift
+//  Art App
+//
+//  Created by Quint DeNiro Fain on 9/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Art_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Art App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Art App
+//
+//  Created by Quint DeNiro Fain on 9/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
